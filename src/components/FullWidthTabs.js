6624611d714c45a3469ec406b22fcc95976cbb52
milad-asghari <milad.asghari@avidarvand.com>
2020-01-23 12:43:19 +0330

    add tabs

--- src/components/FullWidthTabs.js
+++ src/components/FullWidthTabs.js
@@ -1,79 +1,65 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import React from "react";
+import { Tabs } from "antd";
+import LocalCafe from "@material-ui/icons/LocalCafe";
+import Restaurant from "@material-ui/icons/Restaurant";
+import SettingsApplicationsIcon from "@material-ui/icons/SettingsApplications";
+import SearchIcon from "@material-ui/icons/Search";
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `full-width-tab-${index}`,
-    'aria-controls': `full-width-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    backgroundColor: theme.palette.background.paper,
-    width: 500,
-  },
-}));
-
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+const { TabPane } = Tabs;
 export default function FullWidthTabs() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = index => {
-    setValue(index);
-  };
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+    <Tabs defaultActiveKey="1">
+      <TabPane
+        tab={
+          <span>
+            <LocalCafe />
+          </span>
+        }
+        key="1"
+      >
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="ALL" key="4">
+            Tab 1
+          </TabPane>
+          <TabPane tab="PIZZA" key="5">
+            Tab 2
+          </TabPane>
+          <TabPane tab="STEAK" key="6">
+            Tab 3
+          </TabPane>
         </Tabs>
-      </AppBar>
-    </div>
+      </TabPane>
+      <TabPane
+        tab={
+          <span>
+            <Restaurant />
+          </span>
+        }
+        key="2"
+      >
+        Tab 2
+      </TabPane>
+      <TabPane
+        tab={
+          <span>
+            <SettingsApplicationsIcon />
+          </span>
+        }
+        key="3"
+      >
+        Tab 3
+      </TabPane>
+      <TabPane
+        tab={
+          <span>
+            <SearchIcon />
+          </span>
+        }
+        key="4"
+      >
+        Tab 4
+      </TabPane>
+    </Tabs>
   );
 }
