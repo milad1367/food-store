2fa8081362ee11cdd7122737beaa38b7c23c9be1
Ali Tourani <a.tourani1991@gmail.com>
2020-06-11 00:48:14 +0430

    feat: add swipeable tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,31 @@
-import React from 'react';
+import React from "react";
 // Material UI
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 // Components
-import FullWidthTabs from './FullWidthTabs';
+import FullWidthTabs from "./FullWidthTabs";
 
 function App() {
-  const classes = useStyles();
-  return (
-    <Grid className="App" className={classes.root}>
-      <Typography variant="h5" className={classes.appHeader}>
-        Demo App
-      </Typography>
-       <FullWidthTabs />
-    </Grid>
-  );
+	const classes = useStyles();
+	return (
+		<Grid className={classes.root}>
+			<Typography variant="h5" className={classes.appHeader}>
+				Demo App
+			</Typography>
+			<FullWidthTabs />
+		</Grid>
+	);
 }
 
 export default App;
 
 const useStyles = makeStyles({
-  root: {
-    backgroundColor: '#D32F2F',
-    marginTop: '1rem'
-  },
-  appHeader: {
-    textAlign: 'center',
-    color: "#212121",
-  },
+	root: {
+		backgroundColor: "#D32F2F",
+	},
+	appHeader: {
+		textAlign: "center",
+		color: "#212121",
+	},
 });
--- src/components/FullWidthTabs.js
+++ src/components/FullWidthTabs.js
@@ -1,10 +1,10 @@
 import React from "react";
+import SwipeableViews from "react-swipeable-views";
 // Material UI
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 // Icons
 import LocalCafe from "@material-ui/icons/LocalCafe";
 import Restaurant from "@material-ui/icons/Restaurant";
@@ -14,84 +14,133 @@ import SearchIcon from "@material-ui/icons/Search";
 import BeerList from "./BeerList";
 
 export default function FullWidthTabs() {
-  const [tabIndex, setTabIndex] = React.useState(0);
+	const classes = useStyles();
+	const [tabIndex, setTabIndex] = React.useState(0);
+	const [innerTabIndex, setInnerTabIndex] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setTabIndex(newValue);
-  };
+	const handleMainTabChange = (event, newValue) => {
+		setTabIndex(newValue);
+	};
 
-  function TabPanel(props) {
-    const { children, value, index } = props;
-  
-    return (
-      <div
-        hidden={value !== index}
-      >
-        {value === index && (
-          <Box p={3}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
+	const handleMainTabSwipped = (index) => {
+		setTabIndex(index);
+	};
 
-  return (
-    <div>
-    <MainTabs
-    value={tabIndex}
-    indicatorColor="primary"
-    onChange={handleChange}
-  >
-    <MainTab label={<LocalCafe />} />
-    <MainTab label={<Restaurant />} />
-    <MainTab label={<SettingsApplicationsIcon />} />
-    <MainTab label={<SearchIcon />} />
-  </MainTabs>
-  <TabPanel value={tabIndex} index={0}>
-  <BeerList />
-      </TabPanel>
-      <TabPanel value={tabIndex} index={1}>
-  Tab 2
-      </TabPanel>
-      <TabPanel value={tabIndex} index={2}>
-  Tab 3
-      </TabPanel>
-      <TabPanel value={tabIndex} index={3}>
-  Tab 4
-      </TabPanel>
-  </div>
-  );
+	const handleInnerTabChange = (event, newValue) => {
+		setInnerTabIndex(newValue);
+	};
+
+	const handleInnerTabSwipped = (index) => {
+		setInnerTabIndex(index);
+	};
+
+	function TabPanel(props) {
+		const { children, value, index } = props;
+		return (
+			<div hidden={value !== index}>
+				{value === index && (
+					<Grid container className={classes.mainTabContainer}>
+						{children}
+					</Grid>
+				)}
+			</div>
+		);
+	}
+
+	return (
+		<div>
+			<MainTabs
+				value={tabIndex}
+				indicatorColor="primary"
+				onChange={handleMainTabChange}
+			>
+				<MainTab label={<LocalCafe />} />
+				<MainTab label={<Restaurant />} />
+				<MainTab label={<SettingsApplicationsIcon />} />
+				<MainTab label={<SearchIcon />} />
+			</MainTabs>
+			<SwipeableViews index={tabIndex} onChangeIndex={handleMainTabSwipped}>
+				<TabPanel value={tabIndex} index={0}>
+					<InnerTabs
+						value={innerTabIndex}
+						indicatorColor="primary"
+						onChange={handleInnerTabChange}
+						className={classes.innerTabContainer}
+					>
+						<MainTab label="ALL" />
+						<MainTab label="PIZZA" />
+						<MainTab label="STEAK" />
+					</InnerTabs>
+					<SwipeableViews
+						index={innerTabIndex}
+						onChangeIndex={handleInnerTabSwipped}
+					>
+						<TabPanel value={innerTabIndex} index={0}>
+							<BeerList />
+						</TabPanel>
+					</SwipeableViews>
+				</TabPanel>
+				<TabPanel value={tabIndex} index={1}>
+					Tab Foods
+				</TabPanel>
+				<TabPanel value={tabIndex} index={2}>
+					Tab Discounts
+				</TabPanel>
+				<TabPanel value={tabIndex} index={3}>
+					Tab Search
+				</TabPanel>
+			</SwipeableViews>
+		</div>
+	);
 }
 
+const useStyles = makeStyles({
+	mainTabContainer: {
+		backgroundColor: "#FFF",
+		width: "100%",
+		minHeight: "10rem",
+	},
+	innerTabContainer: {
+		width: "100%",
+	},
+});
+
 const MainTabs = withStyles({
-  root: {
-    borderBottom: '1px solid #e8e8e8',
-  },
-  indicator: {
-    backgroundColor: '#000',
-  },
+	root: {},
+	indicator: {
+		backgroundColor: "#000",
+	},
+})(Tabs);
+
+const InnerTabs = withStyles({
+	root: {
+		backgroundColor: "#212121",
+	},
+	indicator: {
+		backgroundColor: "#F0F",
+		display: "none",
+	},
 })(Tabs);
 
 const MainTab = withStyles((theme) => ({
-  root: {
-    color: '#FFF',
-    textTransform: 'none',
-    minWidth: 72,
-    fontWeight: theme.typography.fontWeightRegular,
-    '&:hover': {
-      color: '#FFF',
-      opacity: 1,
-    },
-    '&$selected': {
-      color: '#FFF',
-      backgroundColor: "#212121",
-      borderRadius: '1rem 1rem 0rem 0rem',
-      fontWeight: theme.typography.fontWeightMedium,
-    },
-    '&:focus': {
-      color: '#FFF',
-    },
-  },
-  selected: {},
+	root: {
+		color: "#FFF",
+		textTransform: "none",
+		minWidth: 72,
+		fontWeight: theme.typography.fontWeightRegular,
+		"&:hover": {
+			color: "#FFF",
+			opacity: 1,
+		},
+		"&$selected": {
+			color: "#FFF",
+			backgroundColor: "#212121",
+			borderRadius: "1rem 1rem 0rem 0rem",
+			fontWeight: theme.typography.fontWeightMedium,
+		},
+		"&:focus": {
+			color: "#FFF",
+		},
+	},
+	selected: {},
 }))((props) => <Tab disableRipple {...props} />);
