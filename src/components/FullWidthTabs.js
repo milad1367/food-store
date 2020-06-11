7c5745ab35cad91e90eee2e2f4cd200f5347eed7
Ali Tourani <a.tourani1991@gmail.com>
2020-06-11 16:58:33 +0430

    feat: improve tab foods

--- src/components/FullWidthTabs.js
+++ src/components/FullWidthTabs.js
@@ -78,6 +78,12 @@ export default function FullWidthTabs() {
 						<TabPanel value={innerTabIndex} index={0}>
 							<BeerList />
 						</TabPanel>
+						<TabPanel value={innerTabIndex} index={1}>
+							<BeerList />
+						</TabPanel>
+						<TabPanel value={innerTabIndex} index={2}>
+							<BeerList />
+						</TabPanel>
 					</SwipeableViews>
 				</TabPanel>
 				<TabPanel value={tabIndex} index={1}>
